fe7600b3c7d6d8e134c5a35553d52226750a3d95
Mario Husha <ww-mario@users.noreply.github.com>
2016-02-10 17:04:56 +0000

    Create jqueryui.confirm.js

---
+++ jqueryui.confirm.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function($){
+	$.confirm = function(params){
+		if($('#confirm-dialog').length){
+			$('#confirm-dialog').remove();
+		}
+
+		var markup = [
+			'<div id="confirm-dialog" title="'+params.title+'">' + params.msg,
+			'<span class="ui-icon ui-icon-alert" style="float: left; margin: 0 2px 2px 0;">',
+			'</div>'
+		].join('');
+
+		$(markup).hide().appendTo('body').fadeToggle(99);
+	    $("#confirm-dialog").dialog({
+	      resizable: false,
+	      height:120,
+	      width:350,
+	      modal: true,
+	      buttons: {
+	        "Confirm": function() {
+	        	console.log(params.url);
+	        	document.location.href = params.url;
+	        },
+	        Cancel: function() {
+	          $(this).dialog("close");
+	        }
+	      }
+	    });
+	}
+
+})(jQuery);
