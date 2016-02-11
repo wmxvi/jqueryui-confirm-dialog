04f3802fa0a75b817d13ca5036b6bdbbbd37cb69
Mario Husha <ww-mario@users.noreply.github.com>
2016-02-11 13:11:08 +0000

    Added support for callbacks

--- jqueryui.confirm.js
+++ jqueryui.confirm.js
@@ -1,33 +1,36 @@
 'use strict';
 
 (function($){
-	$.confirm = function(params){
-		if($('#confirm-dialog').length){
-			$('#confirm-dialog').remove();
-		}
+    $.confirm = function(params){
+        if($('#confirm-dialog').length){
+            $('#confirm-dialog').remove();
+        }
 
-		var markup = [
-			'<div id="confirm-dialog" title="'+params.title+'">' + params.msg,
-			'<span class="ui-icon ui-icon-alert" style="float: left; margin: 0 2px 2px 0;">',
-			'</div>'
-		].join('');
+        var markup = [
+            '<div id="confirm-dialog" title="'+params.title+'">' + params.msg,
+            '<span class="ui-icon ui-icon-alert" style="float: left; margin: 0 2px 2px 0;">',
+            '</div>'
+        ].join('');
 
-		$(markup).hide().appendTo('body').fadeToggle(99);
-	    $("#confirm-dialog").dialog({
-	      resizable: false,
-	      height:120,
-	      width:350,
-	      modal: true,
-	      buttons: {
-	        "Confirm": function() {
-	        	console.log(params.url);
-	        	document.location.href = params.url;
-	        },
-	        Cancel: function() {
-	          $(this).dialog("close");
-	        }
-	      }
-	    });
-	}
+        $(markup).hide().appendTo('body').fadeToggle(99);
+        $("#confirm-dialog").dialog({
+          resizable: false,
+          height:120,
+          width:350,
+          modal: true,
+          buttons: {
+            "Confirm": function() {
+                if(typeof params.url == 'function') {
+                  params.url();
+                  return;
+                }
+                document.location.href = params.url;
+            },
+            Cancel: function() {
+              $(this).dialog("close");
+            }
+          }
+        });
+    }
 
 })(jQuery);
